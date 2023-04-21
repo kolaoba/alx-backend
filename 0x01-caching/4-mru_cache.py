9f6794ee23761a8ebbef9c527af1cdb29fd5e86e
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-21 07:09:47 +0100

    add task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""MRU caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Defines the MRU Cache"""
+
+    def __init__(self) -> None:
+        """Initializes the class instance"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            popped_key = list(self.cache_data.keys())[-1]
+            self.cache_data.pop(popped_key)
+            print("DISCARD: {}".format(popped_key))
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            v = self.cache_data.pop(key)
+            self.cache_data[key] = v
+        return self.cache_data.get(key)
